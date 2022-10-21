2df2002ee7d9592b362b7e810e01ffa67ae95881
Seth Lakstins <seth@lakstins.net>
2022-10-20 20:23:24 -0400

    updates to test file

--- db_connection_test.py
+++ db_connection_test.py
@@ -14,5 +14,10 @@ r.ping()
 
 #test orient
 oclient = pyorient.OrientDB("csse@433-10.csse.rose-hulman.edu", 2424)
+client.connect("root", "ich3aeNg")
+#username and password are both admin by default
+client.db_open("TierList", "admin", "admin")
+username = "seth123"
+client.command("SELECT FROM USER WHERE Username=%s" % (username))
 
 
