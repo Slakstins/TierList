1eca0bc645ad44bde978d7d19865bf6c8a0b33ec
Seth Lakstins <seth@lakstins.net>
2022-10-26 15:42:56 -0400

    writing orient ops

--- Queue/orient_ops.py
+++ Queue/orient_ops.py
@@ -21,33 +21,45 @@ def orientCreateUser(inst):
     print("orient created user")
 
 #need to delete the user and all of the tier lists they had created
-#can use deleteTierList to make this a bit cleaner
 def orientDeleteUser(inst):
-    inst["username"]
+    username = inst["username"]
+    #delete all tierlists for the user
+    #this one is a bit complex, need to test it
+    instructions.oclient.command("DELETE VERTEX TIERLIST WHERE in.out[@Class = 'USER'].username='%s'" % (username))
 
-    instructions.oclient.command("DELETE EDGE E WHERE @rid IN (SELECT _in FROM TIERLIST )
+    #delete the outgoing edges from the user
+    instructions.oclient.command("DELETE EDGE E WHERE @rid IN (SELECT out_ FROM USER WHERE username='%s')" % (username))
 
-
-
-    instructions.oclient.command("DELETE VERTEX USER WHERE username=%s" % (inst["username"]))
+    #delete the user
+    instructions.oclient.command("DELETE VERTEX USER WHERE username=%s" % (username))
 
     print("orient deleted user")
 
 def orientUpdateUser(inst):
-    inst["oldUsername"]
-    inst["newUsername"]
-    inst["newSalt"]
-    inst["newHash"]
+    oldUsername = inst["oldUsername"]
+    newUsername = inst["newUsername"]
+    newSalt = inst["newSalt"]
+    newHash = inst["newHash"]
+    instructions.oclient.command("UPDATE USER SET username='%s' salt='%s' hash='%s' WHERE username='%s'" % (
+        newUsername, newSalt, newHash, oldUsername))
     print("orient updated user")
 
 def orientCreateTierList(inst):
+    s = instructions.oclient.command("SELECT FROM TIERLIST WHERE title='%s' AND in.out[@Class = 'USER'].username='%s'"
+            % (inst["title"], inst["username"]))
+
+    res = instructions.oclient.command("CREATE VERTEX TIERLIST CONTENT {title: '%s'})")
+    instructions.oclient.command("CREATE EDGE FROM (SELECT FROM USER WHERE username='%s') TO (SELECT FROM TIERLIST WHERE @rid = '%s')"
+            % (inst["username"], res["@rid"]))
     inst["title"]
     print("orient created tier list")
 
 def orientUpdateTierList(inst):
-    inst["title"]
+    inst["oldTitle"]
     inst["username"]
     inst["tiers"]
+    instructions.oclient.command("UPDATE TIERLIST SET title='%s' tiers=%s WHERE title='%s' AND in.out[@Class='User'].username='%s'"
+            % (inst["newTitle"], inst["tiers"], inst["oldTitle"], inst["username"]
     print("orient updated tier list")
 
 def orientDeleteTierList(inst):
